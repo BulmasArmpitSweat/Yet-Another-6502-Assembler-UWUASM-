22d21e0ec4ccac0077cda19cb161a05161a4e4cd
BulmasArmpitSweat <151138740+BulmasArmpitSweat@users.noreply.github.com>
2024-12-26 14:07:18 +1100

    Added new test file, and hopefully fixed directory
    Added new file, and hopefully fixed that folder that GitHub decided to delete

---
+++ tests/comments.asm
@@ -0,0 +1,14 @@
+// This assembler supports C-style comments
+
+// Like this
+/* And this */
+
+// ; It doesn't support traditional comments though
+// Why? Because fuck you
+
+// I'm joking. I just like C's comments
+
+LDA #$69
+SBC #$42
+/* ; Oh. and it also supports undocumented instructions, although it will complain, and by design */
+JAM
